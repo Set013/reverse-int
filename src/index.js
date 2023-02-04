ca454236aefbac8fafc43ff706a4bdb5097f26cf
Set013 <andreykarnilovi4@gmail.com>
2023-02-04 21:39:56 +0100

    feat: reverse ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) n = -n;
+    return parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
+};
